d4ea4d7ae1c63b2a6c98ed413d9f3d2dc402a178
Max <jmax.barry@gmail.com>
2021-03-23 14:38:02 -0500

    Initial commit

---
+++ src/student_list.py
@@ -0,0 +1,11 @@
+student_first_names = [
+"Ali","Andrew","Angie","Elena",
+"Hunter","Jeffrey","Jonathan",
+"Josh","Karim","Matthew",
+"Ozair","Paul","Prabhakar",
+"Reuben",
+"Sam","Sindhu","William"]
+
+quanggang = ['Christos','Jamie', 'Marcos',
+	    'Rachel','Svitlana'
+	   ] 
